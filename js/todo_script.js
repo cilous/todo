40083e07d96567cc5892b2927c570275f688ea2f
BEN-PC\ben00 <ben.004@hotmail.com>
2016-08-28 01:21:48 +0700

    update beta v0.01

--- js/todo_script.js
+++ js/todo_script.js
@@ -7,16 +7,38 @@ $(document).ready(function(){  // check ready status for this file
   var btn = ".btn-add" // var
   var $btnadd = $(btn)// jquery var
   var $delete = $('.delete')
+  var $btnarc = $('.btn-arc')
+
 
   $btnadd.click(function(){
-    var temp = '<li>' + '<input type="checkbox" class="check">'+ $input.val()+'<span class="delete">[x]</span>' +'</li>'
+    console.log('add')
+    var temp = '<ul>' + '<input type="checkbox" class="check"/>'+'<span class="task">'+ $input.val()+'</span>'+'<span class="delete">[x]</span>' +'</ul>'
     $content.append(temp)
-    console.log($content,temp);
+    //console.log($content,temp)
   })
 
   $delete.click (function() {
-    //console.log($(this),$(this).parent())
-    $(this).parent().remove()
+    //console.log('delete');
+    console.log($(this),$(this).parent())
+    //var x = document.getElementsByClassName('delete')
+    $(this).closest('ul').remove()
+  })
+
+  $btnarc.click(function(){
+    console.log('archieve')
+
+    var boxes = document.getElementsByClassName('check')
+    var texts = document.getElementsByClassName('task')
+    console.log(boxes.length)
+      for(var i = 0; i<boxes.length; i++){
+          var box = boxes[i]
+          var txt = texts[i]
+          if(box.checked) {
+              box.parentNode.remove(box)
+              txt.parentNode.remove(txt)
+              i--
+          }
+      }
   })
 /*
   $input.change(function(){
